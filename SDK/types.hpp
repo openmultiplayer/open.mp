15dfa651ff328be6bab7167c3957e2e3a4bad034
Hual <Hual@users.noreply.github.com>
2021-06-26 18:08:38 +0300

    Add config parsing. Remove GLM from SDK folder. Using the SDK now only requires adding it as a library to the project.

--- SDK/types.hpp
+++ SDK/types.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
 #include <cstdint>
-#include "glm/glm/vec2.hpp"
-#include "glm/glm/vec3.hpp"
-#include "glm/glm/vec4.hpp"
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <nlohmann/json.hpp>
 
 typedef glm::vec2 vector2;
 typedef glm::vec3 vector3;
 typedef glm::vec4 vector4;
-
+typedef nlohmann::json json;
