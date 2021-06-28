b245873f415fb9deed5ac6762140c72dd4add60f
NotUnlikeTheWaves <hleistra@gmail.com>
2021-06-29 00:13:10 +0200

    Tryfix linux build

--- SDK/sdk.hpp
+++ SDK/sdk.hpp
@@ -4,4 +4,9 @@
 
 /// The plugin's entry point function
 /// To be used like so: PLUGIN_ENTRY_POINT(ICore* core) { ... }
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
+    #define SDK_EXPORT __declspec(dllexport)
+#else
+    #define SDK_EXPORT__attribute__((visibility("default")))
+#endif
 #define PLUGIN_ENTRY_POINT extern "C" __declspec(dllexport) bool __cdecl PluginEntryPoint
