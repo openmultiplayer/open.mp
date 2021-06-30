c70d17a8e6051cbb133d5c27ea2242cb53145d72
Hual <Hual@users.noreply.github.com>
2021-06-30 11:04:07 +0300

    Merge events and pool interfaces and implementation in same files.

--- SDK/entity.hpp
+++ SDK/entity.hpp
@@ -8,45 +8,6 @@ struct IIDProvider {
 	virtual int getID() = 0;
 };
 
-/// A statically sized pool interface 
-template <typename T, size_t Count>
-struct IPool {
-	/// Get the first free index or -1 if no index is available to use
-	virtual int findFreeIndex() = 0;
-
-	/// Claim the first free index
-	virtual int claim() = 0;
-
-	/// Attempt to claim the index at hint and if unavailable, claim the first available index
-	virtual int claim(int hint) = 0;
-
-	/// Check if an index is claimed
-	virtual bool valid(int index) = 0;
-
-	/// Get the object at an index
-	virtual T& get(int index) = 0;
-
-	/// Release the object at an index
-	virtual bool release(int index) = 0;
-
-	/// Get a set of all the available objects
-	virtual const std::set<T*>& entries() = 0;
-};
-
-/// A pool with an event dispatcher build in
-template <typename T, size_t Count, class EventHandlerType>
-struct IEventDispatcherPool {
-	using Type = T;
-	static const size_t Cnt = Count;
-	using EventHandler = EventHandlerType;
-
-	/// Get the pool
-	virtual IPool<T, Count>& getPool() = 0;
-
-	/// Get the event dispatcher
-	virtual IEventDispatcher<EventHandlerType>& getEventDispatcher() = 0;
-};
-
 /// A base entity interface
 struct IEntity : public IIDProvider {
 	/// Get the entity's position
--- SDK/events.hpp
+++ SDK/events.hpp
@@ -1,5 +1,11 @@
 #pragma once
 
+#include <set>
+#include <algorithm>
+#include <vector>
+
+/* Interfaces, to be passed around */
+
 /// An event dispatcher
 template <class EventHandlerType>
 struct IEventDispatcher {
@@ -15,4 +21,96 @@ struct IIndexedEventDispatcher {
 	virtual bool addEventHandler(EventHandlerType* handler, size_t index) = 0;
 	virtual bool removeEventHandler(EventHandlerType* handler, size_t index) = 0;
 	virtual bool hasEventHandler(EventHandlerType* handler, size_t index) = 0;
+};
+
+/* Implementation, NOT to be passed around */
+
+template <class EventHandlerType>
+struct EventDispatcher final : public IEventDispatcher<EventHandlerType> {
+    bool addEventHandler(EventHandlerType* handler) override {
+        return handlers.insert(handler).second;
+    }
+
+    bool removeEventHandler(EventHandlerType* handler) override {
+        return handlers.erase(handler) != 0;
+    }
+
+    bool hasEventHandler(EventHandlerType* handler) override {
+        return handlers.find(handler) != handlers.end();
+    }
+
+    template<typename Return, typename ...Params, typename ...Args>
+    void dispatch(Return(EventHandlerType::* mf)(Params...), Args &&... args) {
+        for (EventHandlerType* handler : handlers) {
+            (handler->*mf)(std::forward<Args>(args)...);
+        }
+    }
+
+    template<typename Fn>
+    void all(Fn fn) {
+        std::for_each(handlers.begin(), handlers.end(), fn);
+    }
+
+    template <typename Fn>
+    auto stopAtFalse(Fn fn) {
+        return std::all_of(handlers.begin(), handlers.end(), fn);
+    }
+
+private:
+    std::set<EventHandlerType*> handlers;
+};
+
+template <class EventHandlerType>
+struct IndexedEventDispatcher final : public IIndexedEventDispatcher<EventHandlerType> {
+    IndexedEventDispatcher(size_t max) :
+        handlers(max)
+    { }
+
+    size_t count() override {
+        return handlers.size();
+    }
+
+    bool addEventHandler(EventHandlerType* handler, size_t index) override {
+        if (index >= handlers.size()) {
+            return false;
+        }
+        return handlers[index].insert(handler).second;
+    }
+
+    bool removeEventHandler(EventHandlerType* handler, size_t index) override {
+        if (index >= handlers.size()) {
+            return false;
+        }
+        return handlers[index].erase(handler) != 0;
+    }
+
+    bool hasEventHandler(EventHandlerType* handler, size_t index) override {
+        if (index >= handlers.size()) {
+            return false;
+        }
+        return handlers[index].find(handler) != handlers[index].end();
+    }
+
+    template<typename Return, typename ...Params, typename ...Args>
+    void dispatch(size_t index, Return(EventHandlerType::* mf)(Params...), Args &&... args) {
+        if (index >= Count) {
+            return;
+        }
+        for (EventHandlerType* handler : handlers[index]) {
+            (handler->*mf)(std::forward<Args>(args)...);
+        }
+    }
+
+    template<typename Fn>
+    void all(size_t index, Fn fn) {
+        std::for_each(handlers[index].begin(), handlers[index].end(), fn);
+    }
+
+    template <typename Fn>
+    void stopAtFalse(size_t index, Fn fn) {
+        return std::all_of(handlers[index].begin(), handlers[index].end(), fn);
+    }
+
+private:
+    std::vector<std::set<EventHandlerType*>> handlers;
 };
