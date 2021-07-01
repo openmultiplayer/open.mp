f08f885fd7f67c9c6030705eeb0d4fcc2055c91f
Hual <Hual@users.noreply.github.com>
2021-07-01 08:18:01 +0300

    Revert "Classes plugin. Weapon functions. Bugfixes"

--- SDK/class.hpp
+++ SDK/class.hpp
@@ -1,6 +1,18 @@
 #pragma once
 
-#include <sdk.hpp>
+#include <array>
+#include <pool.hpp>
+
+struct IPlayer;
+
+/// Holds weapon slot data
+struct WeaponSlotData {
+	uint32_t id;
+	uint32_t ammo;
+};
+
+/// An array of weapon slots
+typedef std::array<WeaponSlotData, MAX_WEAPON_SLOTS> WeaponSlots;
 
 /// Player class interface
 struct IClass : public IIDProvider {
@@ -12,19 +24,12 @@ struct IClass : public IIDProvider {
 
 	/// Get the class's spawn position
 	virtual vector3& spawn() = 0;
-
+	
 	/// Get the class's spawn angle
 	virtual float& angle() = 0;
 
 	/// Get the class's spawn weapons
 	virtual WeaponSlots& weapons() = 0;
-};
-
-static const UUID PlayerClassData_UUID = UUID(0x185655ded843788b);
-struct IPlayerClassData : public IPlayerData {
-	PROVIDE_UUID(PlayerClassData_UUID)
-
-	virtual IClass& getClass() = 0;
 };
 
 /// The player class event handler
@@ -35,11 +40,4 @@ struct ClassEventHandler {
 /// The player class pool
 struct IClassPool : public IEventDispatcherPool<IClass, MAX_CLASSES, ClassEventHandler> {
 
-};
-
-static const UUID ClassesPlugin_UUID = UUID(0x8cfb3183976da208);
-struct IClassesPlugin : public IPlugin {
-	PROVIDE_UUID(ClassesPlugin_UUID)
-
-	virtual IClassPool& getClasses() = 0;
 };
