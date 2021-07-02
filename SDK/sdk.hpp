b01703393713d51779656f38e1035fb30cf09cdd
Nikola Yanakiev <nikola.yanakiev0@gmail.com>
2021-07-02 09:01:04 +0300

    Fix framework issue where people could attempt to register network event handlers before network plugins are read. Fix OnPlayerRequestClassResponse structure.

--- SDK/sdk.hpp
+++ SDK/sdk.hpp
@@ -10,5 +10,5 @@
 
 /// The plugin's entry point function
 /// To be used like so: PLUGIN_ENTRY_POINT(ICore* core) { ... }
-typedef IPlugin* (__cdecl* PluginEntryPoint_t)(ICore*);
+typedef IPlugin* (__cdecl* PluginEntryPoint_t)();
 #define PLUGIN_ENTRY_POINT extern "C" SDK_EXPORT IPlugin* __cdecl PluginEntryPoint
--- Server/Components/PlayerDataConsumer/dllmain.cpp
+++ Server/Components/PlayerDataConsumer/dllmain.cpp
@@ -1,9 +1,9 @@
 #include <sdk.hpp>
 #include "../PlayerDataProvider/playerdata.hpp"
 
-ICore* core;
-
 struct MyPlugin : public IPlugin, public PlayerEventHandler {
+	ICore* c = nullptr;
+
 	UUID getUUID() override {
 		return 0x8ee619785a95627e;
 	}
@@ -12,17 +12,26 @@ struct MyPlugin : public IPlugin, public PlayerEventHandler {
 		return "PlayerDataConsumer";
 	}
 
+	void onInit(ICore* core) override {
+		c = core;
+		c->getPlayers().getEventDispatcher().addEventHandler(this);
+	}
+
 	bool onPlayerRequestSpawn(IPlayer& player) override {
 		ISomePlayerData* data = player.queryData<ISomePlayerData>();
 		if (data) {
-			core->printLn("Player %s with some data %i requested spawn", player.name().c_str(), data->getSomeInt());
+			c->printLn("Player %s with some data %i requested spawn", player.name().c_str(), data->getSomeInt());
 		}
 		return true;
 	}
+
+	~MyPlugin() {
+		if (c) {
+			c->getPlayers().getEventDispatcher().removeEventHandler(this);
+		}
+	}
 } plugin;
 
-PLUGIN_ENTRY_POINT(ICore* c) {
-	core = c;
-	core->getPlayers().getEventDispatcher().addEventHandler(&plugin);
+PLUGIN_ENTRY_POINT() {
 	return &plugin;
 }
