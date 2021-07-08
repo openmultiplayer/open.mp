8fdbee1f672e8ed36e6de759705d1249b4085384
Hual <Hual@users.noreply.github.com>
2021-07-08 14:37:14 +0300

    Remove default weapon

--- Server/Components/Classes/dllmain.cpp
+++ Server/Components/Classes/dllmain.cpp
@@ -10,7 +10,6 @@ static const struct DefaultClass final : public PlayerClass {
         spawn = Vector3(0.0f, 0.0f, 3.1279f);
         angle = 0.f;
         weapons.fill(WeaponSlotData{ 0, 0 });
-        weapons[WeaponSlotData{ 22, 50 }.slot()] = WeaponSlotData{ 22, 50 };
     }
 } defClass;
 
