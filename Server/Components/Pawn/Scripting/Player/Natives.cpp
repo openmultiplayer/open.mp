0f272dbc3f2a1a06fd707f59b6b3b7e4b83c67df
besnikskola <rabushajbesnik@gmail.com>
2021-07-12 14:15:28 +0200

    vector3 in params for natives, added natives for play&stop audiostream and createxplosion, added description for createexplosion

--- Server/Components/Pawn/Scripting/Player/Natives.cpp
+++ Server/Components/Pawn/Scripting/Player/Natives.cpp
@@ -8,9 +8,8 @@ SCRIPT_API(SendClientMessage, bool(IPlayer* player, uint32_t colour, std::string
 	return true;
 }
 
-SCRIPT_API(SetPlayerCameraPos, bool(IPlayer* player, float x, float y, float z))
+SCRIPT_API(SetPlayerCameraPos, bool(IPlayer* player, const Vector3& vec))
 {	
-	Vector3 vec(x, y, z);
 	player->setCameraPosition(vec);
 	return true;
 }
@@ -57,9 +56,8 @@ SCRIPT_API(GivePlayerMoney, bool(IPlayer* player, int amount))
 	return true;
 }
 
-SCRIPT_API(SetPlayerCameraLookAt, bool(IPlayer* player, float x, float y, float z, int cutType))
+SCRIPT_API(SetPlayerCameraLookAt, bool(IPlayer* player, const Vector3& vec, int cutType))
 {
-	Vector3 vec(x, y, z);
 	player->setCameraLookAt(vec, cutType);
 	return true;
 }
@@ -75,3 +73,28 @@ SCRIPT_API(SetCameraBehindPlayer, bool(IPlayer* player))
 	player->setCameraBehind();
 	return true;
 }
+
+SCRIPT_API(CreateExplosion, bool(IPlayer* player, const Vector3& vec, int type, float radius))
+{
+	player->createExplosion(vec, type, radius);
+	return true;
+}
+
+SCRIPT_API(CreateExplosion, bool(IPlayer* player, const Vector3& vec, int type, float radius))
+{
+	player->createExplosion(vec, type, radius);
+	return true;
+}
+
+SCRIPT_API(PlayAudioStreamForPlayer, bool(IPlayer* player, std::string const& url, const Vector3& pos, bool usePos, float distance))
+{
+	player->playAudio(url.c_str(), usePos, pos, distance);
+	return true;
+}
+
+SCRIPT_API(StopAudioStreamForPlayer, bool(IPlayer* player))
+{
+	player->stopAudio();
+	return true;
+}
+
