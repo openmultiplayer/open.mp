e79e4f8078a66f76e3eed6fcca7f319578068321
Hual <Hual@users.noreply.github.com>
2021-07-16 15:06:38 +0300

    Revert "Fix RakNet memory leak."
    This reverts commit 6e9a2e6ec5f08b7dd90844d8b5359e31f2b3f5db.

--- lib/RakNet/Source/RakPeer.cpp
+++ lib/RakNet/Source/RakPeer.cpp
@@ -955,7 +955,7 @@ Packet* RakPeer::ReceiveIgnoreRPC( void )
 		while (threadPacket)
 		{
 			packet=*threadPacket;
-			packetSingleProducerConsumer.ReadUnlock(true);
+			packetSingleProducerConsumer.ReadUnlock();
 			threadPacket=packetSingleProducerConsumer.ReadLock();
 			packetPool.Push(packet);
 		}
@@ -3937,7 +3937,7 @@ namespace RakNet
 					if (rcs==rcsFirst)
 					{
 						// Delete the head of the queue
-						rakPeer->requestedConnectionList.ReadUnlock(true);
+						rakPeer->requestedConnectionList.ReadUnlock();
 						rcsFirst=rakPeer->requestedConnectionList.ReadLock();
 						rcs=rcsFirst;
 						continue;
@@ -4001,7 +4001,7 @@ namespace RakNet
 					connectionAttemptCancelled=true;
 					if (rcs==rcsFirst)
 					{
-						rakPeer->requestedConnectionList.ReadUnlock(true);
+						rakPeer->requestedConnectionList.ReadUnlock();
 						rcsFirst=rakPeer->requestedConnectionList.ReadLock();
 						rcs=rcsFirst;
 					}
@@ -4291,7 +4291,7 @@ namespace RakNet
 			bcs->data=0;
 	#endif
 
-			bufferedCommands.ReadUnlock(true);
+			bufferedCommands.ReadUnlock();
 		}
 
 		// Process connection attempts
@@ -4334,7 +4334,7 @@ namespace RakNet
 					// Remove this if possible
 					if (rcs==rcsFirst)
 					{
-						requestedConnectionList.ReadUnlock(true);
+						requestedConnectionList.ReadUnlock();
 						rcsFirst = requestedConnectionList.ReadLock();
 						rcs=rcsFirst;
 					}
