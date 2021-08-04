f24c6c94a342ab4216fcb9d059c4fb2398ab75e8
Amyr Ahmady <hhm6@yahoo.com>
2021-08-04 05:08:57 +0430

    change how natives work based on #87

--- Server/Components/Pawn/Scripting/Actor/Natives.cpp
+++ Server/Components/Pawn/Scripting/Actor/Natives.cpp
@@ -1,13 +1,12 @@
 #include "sdk.hpp"
 #include <iostream>
-#include "Types.hpp"
 #include "../Types.hpp"
 
 SCRIPT_API(CreateActor, int(int modelid, const Vector3& position, float rotation))
 {
-	IActorsComponent* actorsComponent = PawnManager::Get()->components->queryComponent<IActorsComponent>();
-	if (actorsComponent) {
-		IActor* actor = actorsComponent->create(modelid, position, rotation);
+	IActorsComponent* component = PawnManager::Get()->actors;
+	if (component) {
+		IActor* actor = component->create(modelid, position, rotation);
 		if (actor) {
 			return actor->getID();
 		}
@@ -15,33 +14,33 @@ SCRIPT_API(CreateActor, int(int modelid, const Vector3& position, float rotation
 	return INVALID_ACTOR_ID;
 }
 
-SCRIPT_API(DestroyActor, bool(IActor* actor))
+SCRIPT_API(DestroyActor, bool(IActor& actor))
 {
-	IActorsComponent* actorsComponent = PawnManager::Get()->components->queryComponent<IActorsComponent>();
-	if (actorsComponent) {
-		actorsComponent->release(actor->getID());
+	IActorsComponent* component = PawnManager::Get()->actors;
+	if (component) {
+		component->release(actor.getID());
 		return true;
 	}
 	return false;
 }
 
-SCRIPT_API(IsActorStreamedIn, bool(IActor* actor, IPlayer* player))
+SCRIPT_API(IsActorStreamedIn, bool(IActor& actor, IPlayer& player))
 {
-	return player && actor->isStreamedInForPlayer(*player);
+	return actor.isStreamedInForPlayer(player);
 }
 
-SCRIPT_API(SetActorVirtualWorld, bool(IActor* actor, int virtualWorld))
+SCRIPT_API(SetActorVirtualWorld, bool(IActor& actor, int virtualWorld))
 {
-	actor->setVirtualWorld(virtualWorld);
+	actor.setVirtualWorld(virtualWorld);
 	return true;
 }
 
-SCRIPT_API(GetActorVirtualWorld, int(IActor* actor))
+SCRIPT_API(GetActorVirtualWorld, int(IActor& actor))
 {
-	return actor->getVirtualWorld();
+	return actor.getVirtualWorld();
 }
 
-SCRIPT_API(ApplyActorAnimation, bool(IActor* actor, const std::string& animLib, const std::string& animName, float delta, int loop, int lockX, int lockY, int freeze, int time))
+SCRIPT_API(ApplyActorAnimation, bool(IActor& actor, const std::string& animLib, const std::string& animName, float delta, int loop, int lockX, int lockY, int freeze, int time))
 {
 	Animation animation;
 	AnimationTimeData animationData;
@@ -56,64 +55,64 @@ SCRIPT_API(ApplyActorAnimation, bool(IActor* actor, const std::string& animLib, 
 	animation.name = animName;
 	animation.timeData = animationData;
 
-	actor->applyAnimation(animation);
+	actor.applyAnimation(animation);
 	return true;
 }
 
-SCRIPT_API(ClearActorAnimations, bool(IActor* actor))
+SCRIPT_API(ClearActorAnimations, bool(IActor& actor))
 {
-	actor->clearAnimations();
+	actor.clearAnimations();
 	return true;
 }
 
-SCRIPT_API(SetActorPos, bool(IActor* actor, const Vector3& position))
+SCRIPT_API(SetActorPos, bool(IActor& actor, const Vector3& position))
 {
-	actor->setPosition(position);
+	actor.setPosition(position);
 	return true;
 }
 
-SCRIPT_API(GetActorPos, bool(IActor* actor, Vector3* position))
+SCRIPT_API(GetActorPos, bool(IActor& actor, Vector3* position))
 {
-	*position = actor->getPosition();
+	*position = actor.getPosition();
 	return true;
 }
 
-SCRIPT_API(SetActorFacingAngle, bool(IActor* actor, float angle))
+SCRIPT_API(SetActorFacingAngle, bool(IActor& actor, float angle))
 {
-	actor->setRotation(Vector3(0.0f, 0.0f, angle));
+	actor.setRotation(Vector3(0.0f, 0.0f, angle));
 	return true;
 }
 
-SCRIPT_API(GetActorFacingAngle, bool(IActor* actor, float* angle))
+SCRIPT_API(GetActorFacingAngle, bool(IActor& actor, float* angle))
 {
-	*angle = actor->getRotation().ToEuler().z;
+	*angle = actor.getRotation().ToEuler().z;
 	return true;
 }
 
-SCRIPT_API(SetActorHealth, bool(IActor* actor, float health))
+SCRIPT_API(SetActorHealth, bool(IActor& actor, float health))
 {
-	actor->setHealth(health);
+	actor.setHealth(health);
 	return true;
 }
 
-SCRIPT_API(GetActorHealth, bool(IActor* actor, float* health))
+SCRIPT_API(GetActorHealth, bool(IActor& actor, float* health))
 {
-	*health = actor->getHealth();
+	*health = actor.getHealth();
 	return true;
 }
 
-SCRIPT_API(SetActorInvulnerable, bool(IActor* actor, bool invulnerable))
+SCRIPT_API(SetActorInvulnerable, bool(IActor& actor, bool invulnerable))
 {
-	actor->setInvulnerable(invulnerable);
+	actor.setInvulnerable(invulnerable);
 	return true;
 }
 
-SCRIPT_API(IsActorInvulnerable, bool(IActor* actor))
+SCRIPT_API(IsActorInvulnerable, bool(IActor& actor))
 {
-	return actor->isInvulnerable();
+	return actor.isInvulnerable();
 }
 
-SCRIPT_API(IsValidActor, bool(IActor* actor))
+SCRIPT_API(IsValidActor, bool(IActor& actor))
 {
 	return true;
 }
