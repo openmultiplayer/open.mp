2ecbdb42f39993785f42a0ae4f9324e14f5237c7
Amyr Ahmady <hhm6@yahoo.com>
2021-08-04 05:25:12 +0430

    add 3dtextlabel natives

---
+++ Server/Components/Pawn/Scripting/TextLabel/Natives.cpp
@@ -0,0 +1,90 @@
+#include "sdk.hpp"
+#include <iostream>
+#include "../Types.hpp"
+
+SCRIPT_API(Create3DTextLabel, int(const std::string& text, uint32_t colour, const Vector3& position, float drawDistance, int virtualWorld, bool los))
+{
+	ITextLabelsComponent* component = PawnManager::Get()->textlabels;
+	if (component) {
+		ITextLabel* textlabel = component->create(text, Colour::FromRGBA(colour), position, drawDistance, virtualWorld, los);
+		if (textlabel) {
+			return textlabel->getID();
+		}
+	}
+	return INVALID_TEXT_LABEL_ID;
+}
+
+SCRIPT_API(Delete3DTextLabel, bool(ITextLabel& textlabel))
+{
+	ITextLabelsComponent* component = PawnManager::Get()->textlabels;
+	if (component) {
+		component->release(textlabel.getID());
+		return true;
+	}
+	return false;
+}
+
+SCRIPT_API(Attach3DTextLabelToPlayer, bool(ITextLabel& textlabel, IPlayer* player, const Vector3& offset))
+{
+	if (player) {
+		textlabel.attachToPlayer(*player, offset);
+		return true;
+	}
+	return false;
+}
+
+SCRIPT_API(Attach3DTextLabelToVehicle, bool(ITextLabel& textlabel, IVehicle* vehicle, const Vector3& offset))
+{
+	if (vehicle) {
+		textlabel.attachToVehicle(*vehicle, offset);
+		return true;
+	}
+	return true;
+}
+
+SCRIPT_API(Update3DTextLabelText, bool(ITextLabel& textlabel, uint32_t colour, const std::string& text))
+{
+	textlabel.setColour(Colour::FromRGBA(colour));
+	textlabel.setText(text);
+	return true;
+}
+
+SCRIPT_API(CreatePlayer3DTextLabel, int(IPlayer& player, const std::string& text, uint32_t colour, const Vector3& position, float drawDistance, IPlayer* attachedPlayer, IVehicle* attachedVehicle, bool los))
+{
+	IPlayerTextLabelData* labelData = player.queryData<IPlayerTextLabelData>();
+	if (labelData) {
+		IPlayerTextLabel* textlabel = nullptr;
+
+		if (attachedPlayer) {
+			textlabel = labelData->create(text, Colour::FromRGBA(colour), position, drawDistance, los, *attachedPlayer);
+		} 
+		else if (attachedVehicle) {
+			textlabel = labelData->create(text, Colour::FromRGBA(colour), position, drawDistance, los, *attachedVehicle);
+		}
+		else {
+			textlabel = labelData->create(text, Colour::FromRGBA(colour), position, drawDistance, los);
+		}
+
+		if (textlabel) {
+			return textlabel->getID();
+		}
+	}
+	return INVALID_TEXT_LABEL_ID;
+}
+
+SCRIPT_API(DeletePlayer3DTextLabel, bool(IPlayer& player, IPlayerTextLabel& textlabel))
+{
+	IPlayerTextLabelData* labelData = player.queryData<IPlayerTextLabelData>();
+	if (labelData) {
+		labelData->release(textlabel.getID());
+		return true;
+	}
+	return false;
+}
+
+SCRIPT_API(UpdatePlayer3DTextLabelText, bool(IPlayer& player, IPlayerTextLabel& textlabel, uint32_t colour, const std::string& text))
+{
+	textlabel.setColour(Colour::FromRGBA(colour));
+	textlabel.setText(text);
+	return true;
+}
