f1d9a4bf3f772b4459a7b423e7382e9c43aafc60
Amyr Ahmady <hhm6@yahoo.com>
2021-08-05 05:47:31 +0430

    return type bool for ShowPlayerDialog

--- Server/Components/Pawn/Scripting/Dialog/Natives.cpp
+++ Server/Components/Pawn/Scripting/Dialog/Natives.cpp
@@ -3,7 +3,7 @@
 #include "Server/Components/Dialogs/dialogs.hpp"
 #include "../Types.hpp"
 
-SCRIPT_API(ShowPlayerDialog, int(IPlayer& player, int dialogId, int style, const std::string& caption, const std::string& info, const std::string& button1, const std::string& button2))
+SCRIPT_API(ShowPlayerDialog, bool(IPlayer& player, int dialogId, int style, const std::string& caption, const std::string& info, const std::string& button1, const std::string& button2))
 {
 	IPlayerDialogData* dialog = player.queryData<IPlayerDialogData>();
 	if (dialog) {
