64e1b65bd1b3645616d8e65a827fec13878dbaf0
iAmir <amirabgta@gmail.com>
2021-08-17 10:58:49 +0430

    override events from handlers

--- Server/Components/Pawn/Scripting/Player/Events.hpp
+++ Server/Components/Pawn/Scripting/Player/Events.hpp
@@ -4,19 +4,19 @@
 #include "../../Singleton.hpp"
 
 struct PlayerEvents : public PlayerEventHandler, public Singleton<PlayerEvents>{
-	void onConnect(IPlayer & player) {
+	void onConnect(IPlayer & player) override {
 		PawnManager::Get()->CallAll("OnPlayerConnect", player.getID());
 	}
 
-	void onSpawn(IPlayer& player) {
+	void onSpawn(IPlayer& player) override {
 		PawnManager::Get()->CallAll("OnPlayerSpawn", player.getID());
 	}
 
-	bool onCommandText(IPlayer& player, StringView cmdtext) {
+	bool onCommandText(IPlayer& player, StringView cmdtext) override {
 		return PawnManager::Get()->CallAll("OnPlayerCommandText", player.getID(), cmdtext);
 	}
 
-	void onKeyStateChange(IPlayer& player, uint32_t newKeys, uint32_t oldKeys) {
+	void onKeyStateChange(IPlayer& player, uint32_t newKeys, uint32_t oldKeys) override {
 		PawnManager::Get()->CallAll("OnPlayerKeyStateChange", player.getID(), newKeys, oldKeys);
 	}
 };
