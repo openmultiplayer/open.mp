fe5e59770d0aecd5dd1df44043635b0c196a22ee
iAmir <amirabgta@gmail.com>
2021-08-19 14:51:01 +0430

    add checkpoint events

--- Server/Components/Pawn/Scripting/Impl.cpp
+++ Server/Components/Pawn/Scripting/Impl.cpp
@@ -2,15 +2,18 @@
 
 #include "Player/Events.hpp"
 #include "Actor/Events.hpp"
+#include "Checkpoint/Events.hpp"
 
 Scripting::~Scripting() {
 	// remove event handlers
 	PawnManager::Get()->players->getEventDispatcher().removeEventHandler(PlayerEvents::Get());
 	PawnManager::Get()->actors->getEventDispatcher().removeEventHandler(ActorEvents::Get());
+	PawnManager::Get()->checkpoints->getEventDispatcher().removeEventHandler(CheckpointEvents::Get());
 }
 
 void Scripting::addEvents() const {
 	// add event handlers
 	PawnManager::Get()->players->getEventDispatcher().addEventHandler(PlayerEvents::Get());
 	PawnManager::Get()->actors->getEventDispatcher().addEventHandler(ActorEvents::Get());
+	PawnManager::Get()->checkpoints->getEventDispatcher().addEventHandler(CheckpointEvents::Get());
 }
