c4921feb9ca837b4ca3c731612008d4dba01164c
Hual <Hual@users.noreply.github.com>
2021-08-23 14:58:52 +0300

    Correctly handle timer AMX errors. Move free call to timer destructor.

--- Server/Components/Pawn/timers.cpp
+++ Server/Components/Pawn/timers.cpp
@@ -4,9 +4,11 @@ Pair<size_t, PawnTimerHandler*> PawnTimerImpl::newTimer(const char* callback, Mi
 	ITimersComponent* timers = PawnManager::Get()->timers;
 	if (timers && amx) {
 		int funcidx;
-		if (amx_FindPublic(amx, callback, &funcidx) != AMX_ERR_NONE)
+		int err = AMX_ERR_NONE;
+		if ((err = amx_FindPublic(amx, callback, &funcidx)) != AMX_ERR_NONE)
 		{
 			PawnManager::Get()->core->logLn(LogLevel::Warning, "SetTimer(Ex): Couldn't find %s in your script", callback);
+			amx_RaiseError(amx, err);
 		}
 		else {
 			PawnTimerHandler* handler = new PawnTimerHandler(funcidx, amx);
@@ -32,6 +34,7 @@ int PawnTimerImpl::setTimerEx(const char* callback, Milliseconds interval, bool 
 	auto res = newTimer(callback, interval, repeating, amx);
 	PawnTimerHandler* handler = res.second;
 	if (res.second) {
+		int err = AMX_ERR_NONE;
 		handler->fmt = fmt;
 
 		cell* data;
@@ -47,16 +50,18 @@ int PawnTimerImpl::setTimerEx(const char* callback, Milliseconds interval, bool 
 				++i;
 				if (fmt[i] != 'i' && fmt[i] != 'd')
 				{
-					PawnManager::Get()->core->logLn(LogLevel::Error, "SetTimerEx: Error in pushing parameters; Array not followed by size");
-					return 0;
+					err = AMX_ERR_PARAMS;
+					return newTimerExError(handler, amx, err, "Error in pushing parameters; Array not followed by size");
 				}
 				if (
-					amx_GetAddr(amx, params[i - 1], &data) != AMX_ERR_NONE ||
-					amx_GetAddr(amx, params[i], &len1) != AMX_ERR_NONE ||
+					(err = amx_GetAddr(amx, params[i - 1], &data)) != AMX_ERR_NONE ||
+					(err = amx_GetAddr(amx, params[i], &len1)) != AMX_ERR_NONE ||
 					*len1 < 1)
 				{
-					PawnManager::Get()->core->logLn(LogLevel::Error, "SetTimerEx: Error in pushing parameters");
-					return 0;
+					if (err == AMX_ERR_NONE) {
+						err = AMX_ERR_PARAMS;
+					}
+					return newTimerExError(handler, amx, err, "Error in pushing parameters");
 				}
 				// Store the offset in to the new heap data, then the size, then copy the data.
 				handler->params.push_back(handler->data.size() * sizeof(cell));
@@ -64,30 +69,27 @@ int PawnTimerImpl::setTimerEx(const char* callback, Milliseconds interval, bool 
 				handler->data.insert(handler->data.end(), data, data + *len1);
 				break;
 			case 's':
-				if (amx_GetAddr(amx, params[i], &data) != AMX_ERR_NONE || amx_StrSize(data, &len2) != AMX_ERR_NONE)
+				if ((err = amx_GetAddr(amx, params[i], &data)) != AMX_ERR_NONE || (err = amx_StrSize(data, &len2)) != AMX_ERR_NONE)
 				{
-					PawnManager::Get()->core->logLn(LogLevel::Error, "SetTimerEx: Error in pushing parameters");
-					return 0;
+					return newTimerExError(handler, amx, err, "Error in pushing parameters");
 				}
 				// Store the offset in to the new heap data, then copy the data.
 				handler->params.push_back(handler->data.size() * sizeof(cell));
 				handler->data.insert(handler->data.end(), data, data + len2);
 				break;
 			case 'v':
-				if (amx_GetAddr(amx, params[i], &data) != AMX_ERR_NONE)
+				if ((err = amx_GetAddr(amx, params[i], &data)) != AMX_ERR_NONE)
 				{
-					PawnManager::Get()->core->logLn(LogLevel::Error, "SetTimerEx: Error in pushing parameters");
-					return 0;
+					return newTimerExError(handler, amx, err, "Error in pushing parameters");
 				}
 				// Store the offset in to the new heap data, then copy the data.
 				handler->params.push_back(handler->data.size() * sizeof(cell));
 				handler->data.push_back(*data);
 				break;
 			default:
-				if (amx_GetAddr(amx, params[i], &data) != AMX_ERR_NONE)
+				if ((err = amx_GetAddr(amx, params[i], &data)) != AMX_ERR_NONE)
 				{
-					PawnManager::Get()->core->logLn(LogLevel::Error, "SetTimerEx: Error in pushing parameters");
-					return 0;
+					return newTimerExError(handler, amx, err, "Error in pushing parameters");
 				}
 				handler->params.push_back(*data);
 				break;
@@ -95,4 +97,11 @@ int PawnTimerImpl::setTimerEx(const char* callback, Milliseconds interval, bool 
 		}
 	}
 	return res.first;
+}
+
+int PawnTimerImpl::newTimerExError(PawnTimerHandler* handler, AMX* amx, int err, StringView message) {
+	amx_RaiseError(amx, err);
+	PawnManager::Get()->core->logLn(LogLevel::Error, "SetTimerEx: %s", message.data());
+	delete handler;
+	return 0;
 }
