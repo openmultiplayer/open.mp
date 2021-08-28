178f6860da9049f2769de521a6a8a3d1832ddbda
iAmir <amirabgta@gmail.com>
2021-08-28 16:43:41 +0430

    add pickup events

--- Server/Components/Pawn/Scripting/Pickup/Events.hpp
+++ Server/Components/Pawn/Scripting/Pickup/Events.hpp
@@ -1,1 +1,10 @@
 #pragma once
+#include "sdk.hpp"
+#include "../../Manager/Manager.hpp"
+#include "../../Singleton.hpp"
+
+struct PickupEvents : public PickupEventHandler, public Singleton<PickupEvents> {
+	void onPlayerPickUpPickup(IPlayer& player, IPickup& pickup) override {
+		PawnManager::Get()->CallAllInEntryFirst("OnPlayerPickUpPickup", player.getID(), pickup.getID());
+	}
+};
