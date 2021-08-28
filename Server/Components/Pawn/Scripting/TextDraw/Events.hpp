eedaf66854203620cb2a9a6ffacdfa95f8a5121b
iAmir <amirabgta@gmail.com>
2021-08-28 16:31:37 +0430

    add textdraw events

--- Server/Components/Pawn/Scripting/TextDraw/Events.hpp
+++ Server/Components/Pawn/Scripting/TextDraw/Events.hpp
@@ -1,1 +1,20 @@
 #pragma once
+#include "sdk.hpp"
+#include "../../Manager/Manager.hpp"
+#include "../../Singleton.hpp"
+
+struct TextDrawEvents : public TextDrawEventHandler, public Singleton<TextDrawEvents> {
+	void onTextDrawClick(IPlayer& player, ITextDraw& td) override {
+		cell ret = PawnManager::Get()->CallInSidesWhile0("OnPlayerClickTextDraw", player.getID(), td.getID());
+		if (!ret) {
+			PawnManager::Get()->CallInEntry("OnPlayerClickTextDraw", player.getID(), td.getID());
+		}
+	}
+
+	void onPlayerTextDrawClick(IPlayer& player, IPlayerTextDraw& td) override {
+		cell ret = PawnManager::Get()->CallInSidesWhile0("OnPlayerClickPlayerTextDraw", player.getID(), td.getID());
+		if (!ret) {
+			PawnManager::Get()->CallInEntry("OnPlayerClickPlayerTextDraw", player.getID(), td.getID());
+		}
+	}
+};
