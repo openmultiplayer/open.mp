a822252f6e6feda98f76a0262d5ed0a1ad158b08
iAmir <amirabgta@gmail.com>
2021-09-13 05:44:03 +0430

    set AMXFILE env var to {curr_dir}/scriptfiles

--- Server/Components/Pawn/main.cpp
+++ Server/Components/Pawn/main.cpp
@@ -3,6 +3,7 @@
 #include "Manager/Manager.hpp"
 #include "PluginManager/PluginManager.hpp"
 #include "Scripting/Impl.hpp"
+#include "utils.hpp"
 
 struct PawnComponent : public IComponent, public CoreEventHandler {
 	ICore * core = nullptr;
@@ -28,6 +29,29 @@ struct PawnComponent : public IComponent, public CoreEventHandler {
 		PawnManager::Get()->players = &core->getPlayers();
 
 		PawnPluginManager::Get()->core = core;
+
+		// Set AMXFILE environment variable to "{current_dir}/scriptfiles"
+		std::string scriptfilesPath;
+		utils::GetCurrentWorkingDirectory(scriptfilesPath);
+		int length = scriptfilesPath.length();
+
+#ifdef WIN32
+		if (scriptfilesPath[length - 1] != '\\')
+		{
+			scriptfilesPath.append("\\\0");
+		}
+		scriptfilesPath.append("scriptfiles\\");
+		scriptfilesPath.insert(0, "AMXFILE=");
+		_putenv(scriptfilesPath.c_str());
+#else
+		if (scriptfilesPath[length - 1] != '/')
+		{
+			scriptfilesPath.append("/\0");
+		}
+		scriptfilesPath.append("scriptfiles/");
+		setenv("AMXFILE", scriptfilesPath.c_str(), 1);
+#endif
+
 	}
 
 	void onInit(IComponentList* components) override {
