631f940b92969db4460ad77553950688eb1487b8
Hual <Hual@users.noreply.github.com>
2022-01-13 19:50:38 +0200

    AddStaticPickup compatibility fix

--- Server/Components/Pawn/Scripting/Pickup/Natives.cpp
+++ Server/Components/Pawn/Scripting/Pickup/Natives.cpp
@@ -14,16 +14,16 @@ SCRIPT_API(CreatePickup, int(int model, int type, Vector3 position, int virtualW
     return -1;
 }
 
-SCRIPT_API(AddStaticPickup, int(int model, int type, Vector3 position, int virtualWorld))
+SCRIPT_API(AddStaticPickup, bool(int model, int type, Vector3 position, int virtualWorld))
 {
     IPickupsComponent* component = PawnManager::Get()->pickups;
     if (component) {
         IPickup* pickup = component->create(model, type, position, virtualWorld, true);
         if (pickup) {
-            return pickup->getID();
+            return true;
         }
     }
-    return -1;
+    return false;
 }
 
 SCRIPT_API(DestroyPickup, bool(IPickup& pickup))
