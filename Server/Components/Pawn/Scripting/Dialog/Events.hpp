cda7f462d4f0b524a1e181be930f36ba7e3fb6ce
Nikola Yanakiev <nikola.yanakiev0@gmail.com>
2022-01-23 10:48:56 +0200

    Make dialog ID signed. Sync SDK

--- Server/Components/Pawn/Scripting/Dialog/Events.hpp
+++ Server/Components/Pawn/Scripting/Dialog/Events.hpp
@@ -4,7 +4,7 @@
 #include "sdk.hpp"
 
 struct DialogEvents : public PlayerDialogEventHandler, public Singleton<DialogEvents> {
-    void onDialogResponse(IPlayer& player, uint16_t dialogId, DialogResponse response, uint16_t listItem, StringView inputText) override
+    void onDialogResponse(IPlayer& player, int dialogId, DialogResponse response, int listItem, StringView inputText) override
     {
         PawnManager::Get()->CallInSidesWhile0("OnDialogResponse", player.getID(), dialogId, int(response), listItem, inputText);
         PawnManager::Get()->CallInEntry("OnDialogResponse", DefaultReturnValue_False, player.getID(), dialogId, int(response), listItem, inputText);
