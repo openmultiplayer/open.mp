997ad7b6f48ad2086e00b0d23876b5f021572b58
The Cheaterman <the.cheaterman@gmail.com>
2022-02-22 19:35:14 +0100

    Make it a bit prettier

--- Server/Components/Console/console_main.cpp
+++ Server/Components/Console/console_main.cpp
@@ -132,10 +132,9 @@ struct ConsoleComponent final : public IConsoleComponent, public CoreEventHandle
     {
         std::wstring line;
         while (true) {
-            std::getline(std::wcin, line);
             if (
-                threadData->valid
-                && std::wcin
+                std::getline(std::wcin, line)
+                && threadData->valid
             ) {
                 std::scoped_lock<std::mutex> lock(threadData->component->cmdMutex);
 
