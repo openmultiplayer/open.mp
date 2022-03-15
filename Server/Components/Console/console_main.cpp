9b3e5c521fba08808b0419b5bee8a1819a38a5ec
Y_Less <git-athena@y-less.com>
2022-03-15 21:57:59 +0000

    Proper classes in `Console`.

--- Server/Components/Console/console_main.cpp
+++ Server/Components/Console/console_main.cpp
@@ -25,7 +25,8 @@ StringView trim(StringView view)
     return view.substr(start, end - start + 1);
 }
 
-struct ConsoleComponent final : public IConsoleComponent, public CoreEventHandler, public ConsoleEventHandler, public PlayerEventHandler {
+class ConsoleComponent final : public IConsoleComponent, public CoreEventHandler, public ConsoleEventHandler, public PlayerEventHandler {
+private:
     struct ThreadProcData {
         std::atomic_bool valid;
         ConsoleComponent* component;
@@ -95,6 +96,7 @@ struct ConsoleComponent final : public IConsoleComponent, public CoreEventHandle
         }
     } playerRconCommandHandler;
 
+public:
     void sendMessage(IPlayer* player, StringView message) override
     {
         core->logLn(LogLevel::Message, "%s", message.data());
@@ -238,3 +240,4 @@ COMPONENT_ENTRY_POINT()
 {
     return new ConsoleComponent();
 }
+
