9d4face8f87e0783f1cefd566a978f02b7700ad5
Amyr Ahmady <hhm6@yahoo.com>
2022-03-23 07:16:10 +0430

    add pawn events for gangzone enter and leave

--- Server/Components/Pawn/Scripting/GangZone/Events.hpp
+++ Server/Components/Pawn/Scripting/GangZone/Events.hpp
@@ -1,1 +1,16 @@
 #pragma once
+#include "../../Manager/Manager.hpp"
+#include "../../Singleton.hpp"
+#include "sdk.hpp"
+
+struct GangZoneEvents : public GangZoneEventHandler, public Singleton<GangZoneEvents> {
+    void onPlayerEnterGangZone(IPlayer& player, IGangZone& zone) override
+    {
+        PawnManager::Get()->CallAllInEntryFirst("OnPlayerEnterGangZone", DefaultReturnValue_True, player.getID(), zone.getID());
+    }
+
+    void onPlayerLeaveGangZone(IPlayer& player, IGangZone& zone) override
+    {
+        PawnManager::Get()->CallAllInEntryFirst("OnPlayerLeaveGangZone", DefaultReturnValue_True, player.getID(), zone.getID());
+    }
+};
