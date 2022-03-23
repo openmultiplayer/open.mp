6e4eda0e912030e1b27f1bd80570128bce319dce
Amyr Ahmady <hhm6@yahoo.com>
2022-03-23 05:35:17 +0430

    call new gangzone events and new funcs
    Conflicts:
    	Server/Components/GangZones/gangzone.cpp

--- Server/Components/GangZones/gangzone.cpp
+++ Server/Components/GangZones/gangzone.cpp
@@ -2,7 +2,7 @@
 
 using namespace Impl;
 
-class GangZonesComponent final : public IGangZonesComponent, public PlayerEventHandler {
+class GangZonesComponent final : public IGangZonesComponent, public PlayerEventHandler, public PlayerUpdateEventHandler {
 private:
     ICore* core = nullptr;
     MarkedPoolStorage<GangZone, IGangZone, 0, GANG_ZONE_POOL_SIZE> storage;
@@ -23,13 +23,48 @@ public:
     {
         this->core = core;
         this->core->getPlayers().getEventDispatcher().addEventHandler(this);
+        this->core->getPlayers().getPlayerUpdateDispatcher().addEventHandler(this);
     }
 
     ~GangZonesComponent()
     {
         if (core) {
             core->getPlayers().getEventDispatcher().addEventHandler(this);
+            core->getPlayers().getPlayerUpdateDispatcher().addEventHandler(this);
         }
+    }
+
+    bool onUpdate(IPlayer& player, TimePoint now) override
+    {
+        for (auto gangzone : storage._entries()) {
+
+            // getting the list from GangZone implementation instead of interface because
+            // we need the modifiable version
+            FlatHashSet<IPlayer*>& playersInside = reinterpret_cast<GangZone*>(gangzone)->getPlayersInside();
+            const GangZonePos& pos = gangzone->getPosition();
+            const Vector3& playerPos = player.getPosition();
+
+            if (playerPos.x >= pos.min.x && playerPos.x <= pos.max.x && playerPos.y >= pos.min.y && playerPos.y <= pos.max.y && playersInside.find(&player) == playersInside.end()) {
+
+                ScopedPoolReleaseLock<IGangZone> lock(*this, *gangzone);
+                playersInside.insert(&player);
+                eventDispatcher.dispatch(
+                    &GangZoneEventHandler::onPlayerEnterGangZone,
+                    player,
+                    *lock.entry);
+
+            } else if (!(playerPos.x >= pos.min.x && playerPos.x <= pos.max.x && playerPos.y >= pos.min.y && playerPos.y <= pos.max.y) && playersInside.find(&player) != playersInside.end()) {
+
+                ScopedPoolReleaseLock<IGangZone> lock(*this, *gangzone);
+                playersInside.erase(&player);
+                eventDispatcher.dispatch(
+                    &GangZoneEventHandler::onPlayerLeaveGangZone,
+                    player,
+                    *lock.entry);
+            }
+        }
+
+        return true;
     }
 
     IGangZone* create(GangZonePos pos) override
