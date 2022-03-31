fea98bb80307de4c07413c714fb5f80d5350155d
kseny <cristeastefan1999@gmail.com>
2022-03-31 13:46:12 +0300

    Added YSF text labels natives.

--- Server/Components/Pawn/Scripting/TextLabel/Natives.cpp
+++ Server/Components/Pawn/Scripting/TextLabel/Natives.cpp
@@ -39,6 +39,76 @@ SCRIPT_API(Update3DTextLabelText, bool(ITextLabel& textlabel, uint32_t colour, c
     return true;
 }
 
+SCRIPT_API(IsValid3DTextLabel, bool(ITextLabel* textLabel))
+{
+    return textLabel != nullptr;
+}
+
+SCRIPT_API(Is3DTextLabelStreamedIn, bool(IPlayer& player, ITextLabel& textLabel))
+{
+    return textLabel.isStreamedInForPlayer(player);
+}
+
+SCRIPT_API(Get3DTextLabelText, bool(ITextLabel& textLabel, OutputOnlyString& output))
+{
+    output = textLabel.getText();
+    return true;
+}
+
+SCRIPT_API(Get3DTextLabelColor, uint32_t(ITextLabel& textLabel))
+{
+    return textLabel.getColour().RGBA();
+}
+
+SCRIPT_API(Get3DTextLabelPos, bool(ITextLabel& textLabel, Vector3& out))
+{
+    out = textLabel.getPosition();
+    return true;
+}
+
+SCRIPT_API(Set3DTextLabelDrawDistance, bool(ITextLabel& textLabel, float distance))
+{
+    textLabel.setDrawDistance(distance);
+    return true;
+}
+
+SCRIPT_API(Get3DTextLabelDrawDistance, float(ITextLabel& textLabel))
+{
+    return textLabel.getDrawDistance();
+}
+
+SCRIPT_API(Get3DTextLabelLOS, bool(ITextLabel& textLabel))
+{
+    return textLabel.getTestLOS();
+}
+
+SCRIPT_API(Set3DTextLabelLOS, bool(ITextLabel& textLabel, bool status))
+{
+    textLabel.setTestLOS(status);
+    return true;
+}
+
+SCRIPT_API(Get3DTextLabelVirtualWorld, int(ITextLabel& textLabel))
+{
+    return textLabel.getVirtualWorld();
+}
+
+SCRIPT_API(Set3DTextLabelVirtualWorld, int(ITextLabel& textLabel, int world))
+{
+    textLabel.setVirtualWorld(world);
+    return true;
+}
+
+SCRIPT_API(Get3DTextLabelAttachedData, int(ITextLabel& textLabel, int& attached_player, int& attached_vehicle))
+{
+    const TextLabelAttachmentData& data = textLabel.getAttachmentData();
+
+    attached_player = data.playerID;
+    attached_vehicle = data.vehicleID;
+
+    return true;
+}
+
 SCRIPT_API_FAILRET(CreatePlayer3DTextLabel, INVALID_TEXT_LABEL_ID, int(IPlayer& player, const std::string& text, uint32_t colour, Vector3 position, float drawDistance, IPlayer* attachedPlayer, IVehicle* attachedVehicle, bool los))
 {
     IPlayerTextLabelData* labelData = queryExtension<IPlayerTextLabelData>(player);
@@ -70,5 +140,65 @@ SCRIPT_API(UpdatePlayer3DTextLabelText, bool(IPlayer& player, IPlayerTextLabel& 
 {
     textlabel.setColour(Colour::FromRGBA(colour));
     textlabel.setText(text);
+    return true;
+}
+
+SCRIPT_API(IsValidPlayer3DTextLabel, bool(IPlayer& player, IPlayerTextLabel* textLabel))
+{
+    return textLabel != nullptr;
+}
+
+SCRIPT_API(GetPlayer3DTextLabelText, bool(IPlayer& player, IPlayerTextLabel& textLabel, OutputOnlyString& output))
+{
+    output = textLabel.getText();
+    return true;
+}
+
+SCRIPT_API(GetPlayer3DTextLabelColor, uint32_t(IPlayer& player, IPlayerTextLabel& textLabel))
+{
+    return textLabel.getColour().RGBA();
+}
+
+SCRIPT_API(GetPlayer3DTextLabelPos, bool(IPlayer& player, IPlayerTextLabel& textLabel, Vector3& out))
+{
+    out = textLabel.getPosition();
+    return true;
+}
+
+SCRIPT_API(SetPlayer3DTextLabelDrawDist, bool(IPlayer& player, IPlayerTextLabel& textLabel, float distance))
+{
+    textLabel.setDrawDistance(distance);
+    return true;
+}
+
+SCRIPT_API(GetPlayer3DTextLabelDrawDist, float(IPlayer& player, IPlayerTextLabel& textLabel))
+{
+    return textLabel.getDrawDistance();
+}
+
+SCRIPT_API(GetPlayer3DTextLabelLOS, bool(IPlayer& player, IPlayerTextLabel& textLabel))
+{
+    return textLabel.getTestLOS();
+}
+
+SCRIPT_API(SetPlayer3DTextLabelLOS, bool(IPlayer& player, IPlayerTextLabel& textLabel, bool status))
+{
+    textLabel.setTestLOS(status);
+    return true;
+}
+
+SCRIPT_API(GetPlayer3DTextLabelVirtualW, int(IPlayer& player, IPlayerTextLabel& textLabel))
+{
+    // No virtual world support, returning player virtual world.
+    return player.getVirtualWorld();
+}
+
+SCRIPT_API(GetPlayer3DTextLabelAttached, int(IPlayer& player, IPlayerTextLabel& textLabel, int& attached_player, int& attached_vehicle))
+{
+    const TextLabelAttachmentData& data = textLabel.getAttachmentData();
+
+    attached_player = data.playerID;
+    attached_vehicle = data.vehicleID;
+
     return true;
 }
