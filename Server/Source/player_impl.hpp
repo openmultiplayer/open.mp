b6dfc6178c1ecc69bb95557b89ab3aca7bb7ae72
kseny <cristeastefan1999@gmail.com>
2022-04-01 02:22:06 +0300

    Check animation library instead of full anim, since client is crashing only with invalid lib

--- Server/Source/player_impl.hpp
+++ Server/Source/player_impl.hpp
@@ -563,7 +563,7 @@ struct Player final : public IPlayer, public PoolIDProvider, public NoCopy {
 
     void applyAnimation(const AnimationData& animation, PlayerAnimationSyncType syncType) override
     {
-        if (!animationNameValid(animation.lib, animation.name)) {
+        if (!animationLibraryValid(animation.lib)) {
             return;
         }
 
