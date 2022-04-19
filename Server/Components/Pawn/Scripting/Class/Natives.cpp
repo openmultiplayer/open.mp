27c282df8807f2c038c856842c8439e73fd90fb2
Amyr Ahmady <hhm6@yahoo.com>
2022-04-19 05:49:43 +0430

    add GetAvailableClasses & GetPlayerClass natives

--- Server/Components/Pawn/Scripting/Class/Natives.cpp
+++ Server/Components/Pawn/Scripting/Class/Natives.cpp
@@ -77,3 +77,28 @@ SCRIPT_API(GetSpawnInfo, bool(IPlayer& player, int& teamid, int& modelid, Vector
     }
     return false;
 }
+
+SCRIPT_API(GetAvailableClasses, int())
+{
+    IClassesComponent* component = PawnManager::Get()->classes;
+    if (component) {
+        return component->count();
+    }
+    return 0;
+}
+
+SCRIPT_API(GetPlayerClass, bool(IClass& class_, int& teamid, int& skin, Vector3& spawnPos, float& angle, int& weapon1, int& weapon1_ammo, int& weapon2, int& weapon2_ammo, int& weapon3, int& weapon3_ammo))
+{
+    const PlayerClass& data = class_.getClass();
+    teamid = data.team;
+    skin = data.skin;
+    spawnPos = data.spawn;
+    weapon1 = data.weapons[0].id;
+    weapon1_ammo = data.weapons[0].ammo;
+    weapon2 = data.weapons[1].id;
+    weapon2_ammo = data.weapons[1].ammo;
+    weapon3 = data.weapons[2].id;
+    weapon3_ammo = data.weapons[2].ammo;
+    return true;
+}
+
