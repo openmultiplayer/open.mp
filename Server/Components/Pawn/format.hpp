9c9839777b4119febc4bd10bc988adbcb0520346
Y_Less <git-athena@y-less.com>
2022-04-19 17:03:21 +0100

    Add MPL headers to all files.

--- Server/Components/Pawn/format.hpp
+++ Server/Components/Pawn/format.hpp
@@ -1,3 +1,11 @@
+/*
+ *  This Source Code Form is subject to the terms of the Mozilla Public License,
+ *  v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ *  obtain one at http://mozilla.org/MPL/2.0/.
+ *  
+ *  The original code is copyright c 2022, open.mp team and contributors.
+ */
+
 // vim: set ts=4 sw=4 tw=99 noet:
 //
 // AMX Mod X, based on AMX Mod by Aleksander Naszko ("OLO").
