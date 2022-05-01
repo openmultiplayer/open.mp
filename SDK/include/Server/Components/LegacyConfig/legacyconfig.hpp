83583f8ea6f7386267fe83b42e68a10909b56df3
Y_Less <git-athena@y-less.com>
2022-05-01 19:20:22 +0100

    Import SDK config changes.

---
+++ SDK/include/Server/Components/LegacyConfig/legacyconfig.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <component.hpp>
+#include <types.hpp>
+
+static const UID LegacyConfigComponent_UID = UID(0x24ef6216838f9ffc);
+/// The text label component which is a global text label pool
+struct ILegacyConfigComponent : public IComponent
+{
+    PROVIDE_UID(LegacyConfigComponent_UID);
+
+    /// Look up the new config by legacy name (empty if none).
+    virtual StringView getConfig(StringView legacyName) = 0;
+
+    /// Look up the legacy config by new name (empty if none).
+    virtual StringView getLegacy(StringView configName) = 0;
+};
+
---
+++ SDK/include/utils.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+StringView trim(StringView view)
+{
+	char const * const whitespace = " \t\n\r\f\v";
+	const size_t start = view.find_first_not_of(whitespace);
+	if (start == StringView::npos) {
+		return "";
+	}
+	const size_t end = view.find_last_not_of(whitespace);
+	return view.substr(start, end - start + 1);
+}
+
