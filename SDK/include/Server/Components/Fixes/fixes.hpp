4ab6346698ae771e2f022decda4c92151513ae26
Y_Less <git-athena@y-less.com>
2022-06-18 12:35:31 +0100

    Various player fixes extracted from the old fixes.inc-2 PR:
    * Clearing animations works correctly in all cases (looping etc).
    * Clearing animations no longer removes players from vehicles.
    * Setting a parachuting player doesn't kill them.
    * No money deducted on death.
    * Added `RemovePlayerWeapon`, as a nice side-effect of the parachute removal code.
    * Added an optional `force` parameter to `RemovePlayerFromVehicle` because some people abused the `clearAnimations` bug.
    * Added a default spawn point in case no classes are set.
    * Fix players getting stuck in an anima tion on death.
    * Remove random beer bottles and bongs.

--- SDK/include/Server/Components/Fixes/fixes.hpp
+++ SDK/include/Server/Components/Fixes/fixes.hpp
@@ -7,6 +7,9 @@ static const UID FixesData_UID = UID(0x672d5d6fbb094ef7);
 struct IPlayerFixesData : public IExtension
 {
     PROVIDE_EXT_UID(FixesData_UID);
+
+	virtual int getLastCash() const = 0;
+	virtual void setLastCash(int cash) = 0;
 };
 
 static const UID FixesComponent_UID = UID(0xb5c615eff0329ff7);
