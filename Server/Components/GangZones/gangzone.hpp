cd255783311c08ccf9a0099d195bea9469d2c56f
Y_Less <git-athena@y-less.com>
2022-06-28 23:54:28 +0200

    Look up the existing ID first when re-showing a gangzone.

--- Server/Components/GangZones/gangzone.hpp
+++ Server/Components/GangZones/gangzone.hpp
@@ -47,7 +47,10 @@ private:
     void showForClient(IPlayer& player, const Colour& colour) const
     {
         auto data = queryExtension<IPlayerGangZoneData>(player);
-        int id = data->reserveClientID();
+        int id = data->toClientID(poolID);
+        if (id == INVALID_GANG_ZONE_ID) {
+            id = data->reserveClientID();
+        }
         if (id != INVALID_GANG_ZONE_ID) {
             data->setClientID(id, poolID);
             NetCode::RPC::ShowGangZone showGangZoneRPC;
@@ -93,8 +96,11 @@ public:
 
     void showForPlayer(IPlayer& player, const Colour& colour) override
     {
-        col = colour;
+		col = colour;
         const int playerId = player.getID();
+		if (shownFor_.valid(playerId)) {
+            shownFor_.remove(playerId, player);
+        }
         shownFor_.add(playerId, player);
 
         flashingFor_.reset(playerId);
