c4644667a1e6fb70043d1927264f5271d186d5e9
Amyr Ahmady <hhm6@yahoo.com>
2022-07-14 20:38:13 +0430

    fix formatting in #458

--- Server/Source/player_impl.hpp
+++ Server/Source/player_impl.hpp
@@ -520,35 +520,32 @@ struct Player final : public IPlayer, public PoolIDProvider, public NoCopy {
     void setSkin(int skin, bool send = true) override
     {
         skin_ = skin;
-		if (!send)
-		{
-			return;
-		}
-		NetCode::RPC::SetPlayerSkin setPlayerSkinRPC;
-		setPlayerSkinRPC.PlayerID = poolID;
-		setPlayerSkinRPC.Skin = skin;
+        if (!send) {
+            return;
+        }
+        NetCode::RPC::SetPlayerSkin setPlayerSkinRPC;
+        setPlayerSkinRPC.PlayerID = poolID;
+        setPlayerSkinRPC.Skin = skin;
         IPlayerVehicleData* data = queryExtension<IPlayerVehicleData>(*this);
-        if (data)
-		{
+        if (data) {
             IVehicle* vehicle = data->getVehicle();
-            if (vehicle)
-			{
-				// `SetPlayerSkin` fails in vehicles, so remove them, set the skin, and put them back in again
-				// in quick succession.
-				int seat = data->getSeat();
-				removeFromVehicle(true);
-				PacketHelper::broadcastToStreamed(setPlayerSkinRPC, *this, false /* skipFrom */);
-				// Put them back in the vehicle, but don't involve the vehicle subsystem (it does a
-				// load of other checks we know aren't required here).
-				NetCode::RPC::PutPlayerInVehicle putPlayerInVehicleRPC;
-				putPlayerInVehicleRPC.VehicleID = vehicle->getID();
-				putPlayerInVehicleRPC.SeatID = seat;
-				PacketHelper::send(putPlayerInVehicleRPC, *this);
-				// End early.
-				return;
+            if (vehicle) {
+                // `SetPlayerSkin` fails in vehicles, so remove them, set the skin, and put them back in again
+                // in quick succession.
+                int seat = data->getSeat();
+                removeFromVehicle(true);
+                PacketHelper::broadcastToStreamed(setPlayerSkinRPC, *this, false /* skipFrom */);
+                // Put them back in the vehicle, but don't involve the vehicle subsystem (it does a
+                // load of other checks we know aren't required here).
+                NetCode::RPC::PutPlayerInVehicle putPlayerInVehicleRPC;
+                putPlayerInVehicleRPC.VehicleID = vehicle->getID();
+                putPlayerInVehicleRPC.SeatID = seat;
+                PacketHelper::send(putPlayerInVehicleRPC, *this);
+                // End early.
+                return;
             }
         }
-		// Not on a bike, the normal set works.
+        // Not on a bike, the normal set works.
         PacketHelper::broadcastToStreamed(setPlayerSkinRPC, *this, false /* skipFrom */);
     }
 
