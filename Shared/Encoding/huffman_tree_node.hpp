4950dfaf71ae72dd2eb74654f0d421d1238da410
Y_Less <git-athena@y-less.com>
2022-07-31 12:21:05 +0100

    Remove copyright headers from library files that shouldn't have them.

--- Shared/Encoding/huffman_tree_node.hpp
+++ Shared/Encoding/huffman_tree_node.hpp
@@ -1,11 +1,3 @@
-/*
- *  This Source Code Form is subject to the terms of the Mozilla Public License,
- *  v. 2.0. If a copy of the MPL was not distributed with this file, You can
- *  obtain one at http://mozilla.org/MPL/2.0/.
- *
- *  The original code is copyright (c) 2022, open.mp team and contributors.
- */
-
 /// \file
 /// \brief \b [Internal] A single node in the Huffman Encoding Tree.
 ///
