e8b6bd21f9ca003d4a7cd1dcafed8d790f318271
kseny <cristeastefan1999@gmail.com>
2022-08-02 12:38:33 +0300

    1 more broken cam mode

--- Server/Source/player_pool.hpp
+++ Server/Source/player_pool.hpp
@@ -671,7 +671,7 @@ struct PlayerPool final : public IPlayerPool, public NetworkEventHandler, public
                 player.aimingData_.aspectRatio = (aimSync.AspectRatio * 1.f / 255) + 1.f;
 
                 // Fix for camera shaking hack, i think there are more bugged ids
-                if (aimSync.CamMode == 34u || aimSync.CamMode == 45u || aimSync.CamMode == 41u || aimSync.CamMode == 42u)
+                if (aimSync.CamMode == 34u || aimSync.CamMode == 45u || aimSync.CamMode == 41u || aimSync.CamMode == 42u || aimSync.CamMode == 49u)
                     aimSync.CamMode = 4u;
 
                 aimSync.PlayerID = player.poolID;
