b0d416b2de5f95ca841e526112c6ac16d27736a4
The Cheaterman <the.cheaterman@gmail.com>
2022-08-26 22:46:26 +0200

    Sanitize docker setup, use Ubuntu 22.04 for CI

---
+++ docker/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Available configs: Debug, [RelWithDebInfo], Release
+config=RelWithDebInfo
+# Available versions: 18.04, [22.04]
+ubuntu_version=22.04
+
+
+docker build \
+    -t open.mp/build:ubuntu-${ubuntu_version} \
+    build_ubuntu-${ubuntu_version}/ \
+|| exit 1
+
+if [[ ! -d './conan' ]]; then
+    mkdir conan &&
+    chown 1000:1000 conan || exit 1
+fi
+
+docker run \
+    --rm \
+    -ti \
+    -w /code \
+    -v $PWD/..:/code \
+    -v $PWD/conan:/home/user/.conan \
+    -e CONFIG=${config} \
+    -e OMP_BUILD_VERSION=$(git rev-list $(git rev-list --max-parents=0 HEAD) HEAD | wc -l) \
+    -e OMP_BUILD_COMMIT=$(git rev-parse HEAD) \
+    open.mp/build:ubuntu-${ubuntu_version}
