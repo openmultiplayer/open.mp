45f7b4afde7e30b87b379ac2ac7f8086cf36418b
ksenonadv <cristeastefan1999@gmail.com>
2022-08-26 19:40:12 +0300

    ignore setSpectating calls with current state

--- Server/Source/player_impl.hpp
+++ Server/Source/player_impl.hpp
@@ -562,6 +562,10 @@ struct Player final : public IPlayer, public PoolIDProvider, public NoCopy {
 
     void setSpectating(bool spectating) override
     {
+        if (spectating == spectateData_.spectating) {
+            return;
+        }
+
         if (!spectating) {
             toSpawn_ = true;
             spectateData_.type = PlayerSpectateData::ESpectateType::None;
