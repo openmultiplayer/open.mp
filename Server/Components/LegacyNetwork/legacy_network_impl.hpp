32d3b848eaa0998a6f01fe6c97b8bda621553a53
ksenonadv <cristeastefan1999@gmail.com>
2022-08-26 19:25:19 +0300

    better rule name

--- Server/Components/LegacyNetwork/legacy_network_impl.hpp
+++ Server/Components/LegacyNetwork/legacy_network_impl.hpp
@@ -25,8 +25,8 @@ using namespace Impl;
 
 #define MAGNITUDE_EPSILON 0.00001f
 
-static const StaticArray<StringView, 1> ProtectedRules = {
-    { "version" },
+static const StaticArray<StringView, 2> ProtectedRules = {
+    "version", "allow_037"
 };
 
 class Core;
