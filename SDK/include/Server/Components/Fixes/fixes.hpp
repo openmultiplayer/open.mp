d0b615e1c486569644ac7a3a41e56d372440a0cb
Y_Less <git-athena@y-less.com>
2022-09-27 11:03:18 +0100

    Switching to using component not data.

--- SDK/include/Server/Components/Fixes/fixes.hpp
+++ SDK/include/Server/Components/Fixes/fixes.hpp
@@ -13,7 +13,6 @@
 static const UID FixesData_UID = UID(0x672d5d6fbb094ef7);
 struct IPlayerFixesData : public IExtension
 {
-	virtual void fixAnimationLibrary(IPlayer* player, IActor* actor, AnimationData const* animation) = 0;
 	PROVIDE_EXT_UID(FixesData_UID);
 
 	virtual bool sendGameText(StringView message, Milliseconds time, int style) = 0;
@@ -22,6 +21,8 @@ struct IPlayerFixesData : public IExtension
 static const UID FixesComponent_UID = UID(0xb5c615eff0329ff7);
 struct IFixesComponent : public IComponent
 {
+	virtual void applyAnimation(IPlayer& whom, IPlayer* player, IActor* actor, AnimationData const* animation) = 0;
+	virtual void clearAnimation(IPlayer* player, IActor* actor) = 0;
 	PROVIDE_UID(FixesComponent_UID);
 
 	virtual bool sendGameText(StringView message, Milliseconds time, int style) = 0;
