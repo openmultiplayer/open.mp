981b489ab17685c5dfa7de4f627ef969fdddf363
WoutProvost <wout_provost@hotmail.com>
2023-01-08 17:56:01 +0100

    Update readme with conceptual descriptions

--- SDK/include/gtaquat.hpp
+++ SDK/include/gtaquat.hpp
@@ -7,7 +7,7 @@
 // WARNING: Do not use glm::quat or glm::vec4 for quaternions instead of this class!
 // The game uses a weird implementation of Euler angles and/or quaternions than almost everything you'd find.
 // Special thanks to IllidanS4 and his i_quat include for the formulas used in this class.
-// See this issue for more information: https://github.com/openmultiplayer/open.mp/issues/143
+// See this issue for more information: https://github.com/openmultiplayer/old.open.mp/issues/143
 // See this link for an explanation of the formulas: https://math.stackexchange.com/questions/1477926/quaternion-to-euler-with-some-properties
 
 class GTAQuat
