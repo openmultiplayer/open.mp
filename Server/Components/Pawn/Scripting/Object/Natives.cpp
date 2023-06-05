2516b7e0c46207aac808ff1e8752e453b58c8dd2
Hual <nikola.yanakiev0@gmail.com>
2023-06-05 17:12:27 +0300

    Add SetObjectsDefaultCameraCollision native to sync with omp-stdlib

--- Server/Components/Pawn/Scripting/Object/Natives.cpp
+++ Server/Components/Pawn/Scripting/Object/Natives.cpp
@@ -225,6 +225,11 @@ SCRIPT_API(SetObjectsDefaultCameraCol, bool(bool disable))
 	return false;
 }
 
+SCRIPT_API(SetObjectsDefaultCameraCollision, bool(bool disable))
+{
+	return NATIVE_SetObjectsDefaultCameraCol<bool>(disable);
+}
+
 SCRIPT_API(GetObjectDrawDistance, float(IObject& object))
 {
 	return object.getDrawDistance();
