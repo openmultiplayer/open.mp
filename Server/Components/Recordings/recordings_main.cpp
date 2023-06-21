97ed95417cc816bda8b9aaee9db55aa0d345785d
Hual <nikola.yanakiev0@gmail.com>
2023-06-21 15:04:07 +0300

    Flip checks

--- Server/Components/Recordings/recordings_main.cpp
+++ Server/Components/Recordings/recordings_main.cpp
@@ -98,7 +98,7 @@ private:
 			}
 
 			// Write on foot recording data
-			if (data->file_.good() && data->type_ == PlayerRecordingType_OnFoot)
+			if (data->type_ == PlayerRecordingType_Driver && data->file_.good())
 			{
 				const uint32_t timeSinceRecordStart = duration_cast<Milliseconds>(Time::now() - data->start_).count();
 				data->file_.write(reinterpret_cast<const char*>(&timeSinceRecordStart), sizeof(uint32_t));
@@ -148,7 +148,7 @@ private:
 			}
 
 			// Write driver recording data
-			if (data->file_.good() && data->type_ == PlayerRecordingType_Driver)
+			if (data->type_ == PlayerRecordingType_Driver && data->file_.good())
 			{
 				uint32_t timeSinceRecordStart = duration_cast<Milliseconds>(Time::now() - data->start_).count();
 				data->file_.write(reinterpret_cast<const char*>(&timeSinceRecordStart), sizeof(uint32_t));
