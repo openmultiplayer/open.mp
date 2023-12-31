52da993ddf8503d67abd7a3748fe297bc337ec71
Alex Cole <ac@ytterate.dev>
2023-12-31 17:31:14 +0100

    Missing MPL headers.

--- SDK/include/Server/Components/Pawn/Impl/pawn_impl.hpp
+++ SDK/include/Server/Components/Pawn/Impl/pawn_impl.hpp
@@ -1,3 +1,11 @@
+/*
+ *  This Source Code Form is subject to the terms of the Mozilla Public License,
+ *  v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ *  obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ *  The original code is copyright (c) 2022, open.mp team and contributors.
+ */
+
 #pragma once
 
 // This file should only be included in one place.
