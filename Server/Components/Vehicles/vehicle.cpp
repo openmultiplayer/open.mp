59c91acf5d8812d5cfd22dc12fef474c287bbe5d
Nexius <nexius.beslam@mail.ru>
2024-09-21 00:00:37 +0300

    Check for non-streamed trailer

--- Server/Components/Vehicles/vehicle.cpp
+++ Server/Components/Vehicles/vehicle.cpp
@@ -233,7 +233,15 @@ bool Vehicle::updateFromDriverSync(const VehicleDriverSyncPacket& vehicleSync, I
 			trailer = static_cast<Vehicle*>(pool->get(vehicleSync.TrailerID));
 			if (trailer)
 			{
-				trailer->cab = this;
+				const bool isStreamedIn = trailer->isStreamedInForPlayer(player);
+				if (!isStreamedIn)
+				{
+					trailer = nullptr;
+				}
+				else
+				{
+					trailer->cab = this;
+				}
 			}
 		}
 	}
