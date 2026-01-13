0b9a02c5444c0ee89c526fffb2ec418967572fa7
ksenonadv <cristeastefan1999@gmail.com>
2026-01-13 02:07:09 +0200

    check if leaving spectator mode in PlayerRequestSpawnRPC

--- Server/Source/player_pool.hpp
+++ Server/Source/player_pool.hpp
@@ -62,7 +62,7 @@ struct PlayerPool final : public IPlayerPool, public NetworkEventHandler, public
 		bool onReceive(IPlayer& peer, NetworkBitStream& bs) override
 		{
 			PlayerState state = peer.getState();
-			if (state == PlayerState_Spawned || (state >= PlayerState_OnFoot && state < PlayerState_Wasted))
+			if (!peer.isLeavingSpectatorMode() && (state == PlayerState_Spawned || (state >= PlayerState_OnFoot && state < PlayerState_Wasted)))
 			{
 				return false;
 			}
